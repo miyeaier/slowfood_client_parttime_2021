5837ea2a5d263343cd789ab202e0fdc383d0c3bc
maxarvid <max.arvid.anderson@gmail.com>
2022-02-15 11:37:47 +0100

    Refactor cypress intercept, refactors state assignment to mirror fixure data structure

--- cypress/integration/UserCanViewProducts.feacture.js
+++ cypress/integration/UserCanViewProducts.feacture.js
@@ -1,20 +1,20 @@
-describe('Display list of products', () => {
+describe("Display list of products", () => {
   beforeEach(() => {
-    cy.visit('/')
-    cy.intercept('GET', 'https://reqres.in/api/food', { fixture: 'products.json' }).as(
-      'getProducts',
-    )
-  })
+    cy.intercept("GET", "**/api/products", {
+      fixture: "products.json",
+    }).as("getProducts");
+    cy.visit("/");
+  });
 
-  it('is expected to see a header', () => {
-    cy.get('[data-cy=header]').should('be.visible')
-  })
+  it("is expected to see a header", () => {
+    cy.get("[data-cy=header]").should("be.visible");
+  });
 
-  it('displays a list with 3 items', () => {
-    cy.get('[data-cy=products-list]').should('have.length', 1)
-  })
+  it("displays a list with 3 items", () => {
+    cy.get("[data-cy=products-list]").should("have.length", 1);
+  });
 
-  it('the list items display the expected content', () => {
-    cy.get('[data-cy=products-list]').first().should('contain', 'cerulean')
-  })
-})
+  it("the list items display the expected content", () => {
+    cy.get("[data-cy=products-list]").first().should("contain", "cerulean");
+  });
+});
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,25 +1,22 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-//import { api } from "./network"
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([])
-    const fetchProducts = async () => {
-      const response = await axios.get('https://reqres.in/api/food')
-      setProducts(response.data.data)
-    }
-  
-    useEffect(() => { 
-      fetchProducts()
-    },[]);
-  
-      const productList = products.map((product) => {
-        return <li key={product.id}>{`${product.name} ${product.price}`} </li>
-      })
-      return <ul id="products-list">
-        {productList}
-        </ul>
-    }
+  const fetchProducts = async () => {
+    const response = await axios.get("https://reqres.in/api/products");
+    setProducts(response.data.products);
+  };
 
-export default Products
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const productList = products.map((product) => {
+    return <li key={product.id}>{`${product.name} ${product.price}`} </li>;
+  });
+  return <ul id="products-list">{productList}</ul>;
+};
+
+export default Products;
