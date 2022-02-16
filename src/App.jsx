aaf99a2667a2a4450fc0df8cd4a1af300b09d708
miyeaier <miyesier@gmail.com>
2022-02-16 22:52:25 +0100

    delete home.jsx and add backgroundimage on app.js container

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import Header from "./Header";
 import Products from "./Products";
-import Home from "./Home";
 import Footer from "./Footer";
 import "./index.css";
 import { Container } from "semantic-ui-react";
 
 const App = () => {
   return (
-    <Container>
+    <Container className="BackgroundImage">
       <Header />
       <div data-cy="products-list">
         <h3> Menu</h3>
         <Products />
       </div>
       <Footer />
-      <Home />
     </Container>
   );
 };
--- src/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./index.css";
-
-const Home = () => {
-  return <div className="BackgroundImage"></div>;
-};
-
-export default Home;
