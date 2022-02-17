1448bceb273c6c77850726bef5f5a3b4c4172608
gsim1903 <gavin@internetbusker.co>
2022-02-17 11:46:09 +0100

    category refactor

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,33 +1,34 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { List } from "semantic-ui-react";
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { List } from 'semantic-ui-react'
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([])
 
   const fetchProducts = async () => {
-    const response = await axios.get("https://reqres.in/api/products");
-    setProducts(response.data.products);
-  };
+    const response = await axios.get('https://reqres.in/api/products')
+    setProducts(response.data.products)
+  }
 
   useEffect(() => {
-    fetchProducts();
-  }, []);
+    fetchProducts()
+  }, [])
 
+  let prevCategory = ''
   const productList = products
+    .sort((a, b) => (a.category < b.category ? 1 : -1))
+    .map((product) => {
+      return (
+        <>
+          <List.Item key={product.id}>
+            {`${product.name} ${product.price}`}
+          </List.Item>
+          {prevCategory = product.category}
+        </>
+      )
+      
+    })
+  return <List id="products-list">{productList}</List>
+}
 
-  .sort((a,b) => a.category < b.category ? 1:-1)
-  .map((product) => {
-    return (
-      <List.Item key={product.id}>
-        {`${product.category}  -  ${product.name} ${product.price}` }
-      </List.Item>
-    );
-
-  });
-  return <List id="products-list">{productList}</List>;
-};
-
-
-
-export default Products;
+export default Products
