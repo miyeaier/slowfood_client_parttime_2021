395f3dbbe26c1db0003a0e10a2a969135a91c286
maxarvid <max.arvid.anderson@gmail.com>
2022-02-23 16:41:55 +0100

    adds review order component with semantic button

---
+++ src/ReviewOrder.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const ReviewOrder = () => {
+  return (
+    <Button data-cy="show-order" inverted>ReviewOrder</Button>
+  )
+}
+
+export default ReviewOrder
