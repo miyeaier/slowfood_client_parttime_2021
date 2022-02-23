281636d7e9eb6792c3a780ede4cc8d72fd5afeb5
maxarvid <max.arvid.anderson@gmail.com>
2022-02-23 16:15:46 +0100

    adds test file

---
+++ cypress/integration/userReviewOrder.feature.js
@@ -0,0 +1,31 @@
+describe("Display products in order", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/api/products", {
+      fixture: "products.json",
+    }).as("getProducts");
+    cy.intercept("GET", "**/api/orders/**", {
+      fixture: "order.json",
+    }).as("getOrder");
+    cy.visit("/");
+  });
+
+  describe("successfully", () => {
+    it("is expected to show a list of products", () => {
+      cy.get("[data-cy=show-order]").click();
+      cy.get("[data-cy=order-list]").children().should("have.length", 3);
+    });
+
+    it("is expected to include a product name", () => {
+      cy.get("[data-cy=order-list")
+        .children()
+        .last()
+        .within(() => {
+          cy.contains("Chicken wrap").should("be.visible");
+        });
+    });
+
+    it("is expected to calculate the total cost", () => {
+      cy.get("[data-cy=total-price]").should("contain", "550kr");
+    });
+  });
+});
