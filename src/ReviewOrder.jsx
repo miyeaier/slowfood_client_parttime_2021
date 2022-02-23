457c1ea7e41fd64db35e44475627bc72a3b93338
miyeaier <miyesier@gmail.com>
2022-02-23 17:00:21 +0100

    adds Modal review orders

--- src/ReviewOrder.jsx
+++ src/ReviewOrder.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { Button } from 'semantic-ui-react'
+import React, { useState } from "react";
+import { Button, Modal } from "semantic-ui-react";
 
 const ReviewOrder = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <Button data-cy="show-order" inverted>ReviewOrder</Button>
-  )
-}
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      trigger={
+        <Button data-cy="show-order" inverted>
+          Review Order
+        </Button>
+      }
+    >
+      <Modal.Header>Select a Photo</Modal.Header>
+    </Modal>
+  );
+};
 
-export default ReviewOrder
+export default ReviewOrder;
